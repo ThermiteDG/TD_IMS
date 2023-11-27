59a7cfe614b37e5ab15ff96a8e53f1941cf0a405
ViridianAStar <88561900+ViridianAStar@users.noreply.github.com>
2023-11-27 10:13:07 -0500

    Delete a thing

--- bananaPhone.ps1
+++
@@ -1,6 +0,0 @@
-$currentPath = $MyInvocation.MyCommand.Path 
-$parent = Split-Path -Path $currentPath -parent
-$test = $parent+'\Banan.png'
-Write-Output $test
-Set-ItemProperty -path 'HKCU:ControlPanel/Desktop' -name WallPaper -value $test
-do {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters, 1, True Start-Process "https://youtu.be/42JCu9jWmY0"} while ($true)
